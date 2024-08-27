93477512ba2fbee3fbc0ebb85f8b203d0084c3b5
Sean Bradley <s_bradley67@hotmail.com>
2024-08-27 15:03:06 -0700

    Basic JavaScript Projects Assignment

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,32 @@
+// Script Assignment
+// alert("Hello, World!")
+
+// window.alert() Method Assignment
+window.alert("Hello, World!");
+
+// document.write() Method Assignment
+document.write("Hello, World!");
+
+// Variable Assignment - Also works for concatenated string challenge because I wanted this to display on a new line and we havent covered that yet.
+var A = "This is a string.";
+document.write("<br>" + A);
+
+// String Assignment
+var B = "Hello, World! This is a string.";
+window.alert(B);
+
+// Escape Character Assignment
+document.write("<br>Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
+
+// Concatenation Assignment
+document.write("<br>\"Be who you are and say what you feel,"
+            + " because those who mind don't matter and those who matter don't mind.\""
+            + " - Dr. Seuss");
+
+// Multiple Variables Assignment
+var Family = "The Bradleys", Dad = "John", Mother = "Kathleen", Son1 = "Ryan", Son2 = "Sean";
+document.write("<br>" + Son2 + "<br>");
+
+// Expressions Assignment
+document.write(3 + 3);
+
